aa8dd2aba9248286a1b8f05bbb4f41bf57a0e2d5
Timofii <gerdaumka@gmail.com>
2020-09-22 15:50:15 +0300

    add fix namess

--- Task_1/data_collection.py
+++ Task_1/data_collection.py
@@ -4,12 +4,12 @@ import sqlite3
 html = open('UEEX.html').read()
 soup = BeautifulSoup(html, "html.parser")
 div_value = soup.find_all('div', class_='value')
-table_data = [item.contents[0].strip() for item in div_value[:95]]
-del table_data[12], table_data[30], table_data[48], table_data[66], table_data[84]
-rrr = [tuple(table_data[i:i+18]) for i in range(0, len(table_data), 18)]
+list_data = [item.contents[0].strip() for item in div_value[:95]]
+del list_data[12], list_data[30], list_data[48], list_data[66], list_data[84]
+main_data = [tuple(list_data[i:i+18]) for i in range(0, len(list_data), 18)]
 
 conn = sqlite3.connect('dtrading')
 cur = conn.cursor()
 sql_statement = 'INSERT INTO Energy VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
-cur.executemany(sql_statement, rrr)
+cur.executemany(sql_statement, main_data)
 conn.commit()
