85ac61459434ce28331de0a810ef5b08bab1dfc6
Timofii <gerdaumka@gmail.com>
2020-09-25 17:13:12 +0300

    fix Task_1

--- Task_1/data_collection.py
+++ Task_1/data_collection.py
@@ -7,7 +7,7 @@ div_value = soup.find_all('div', class_='value')
 list_data = [item.contents[0].strip() for item in div_value[:95]]
 # In the previous version I used del to quickly remove duplicates '1 500.00' in list
 # del list_data[12], list_data[30], list_data[48], list_data[66], list_data[84]
-# Of course I know better way ... Sorry)
+# Of course I know better way ...
 new_list_data = [j for i, j in enumerate(list_data) if i not in [12, 31, 50, 69, 88]]
 main_data = [tuple(new_list_data[i:i + 18]) for i in range(0, len(new_list_data), 18)]
 
@@ -18,12 +18,12 @@ cur.executemany(sql_statement, main_data)
 conn.commit()
 
 # More simple example with requests (I didn't notice him)
-# If carefully use developer tools, we can find better link. I'll show you the main idea.
+# I had a problem with getting the value, but if we use developer tools carefully we can find a better link.
+# I'll show you the main idea.
 # import requests
 #
-# url = 'https://epbets.ueex.com.ua/public/PublicHandler.ashx?CommandName=jPositions&id_auc=3294127&info=Y&lan=ua&fl_FullNumber=&fl_Direction=&fl_Owner=&fl_Goods=&fl_DeliveryType=&empty_filter=Y'
+# url = 'https://epbets.ueex.com.ua/public/PublicHandler.ashx?CommandName=jPositions&id_auc=3294127&info=Y&lan=ua' \
+#       '&fl_FullNumber=&fl_Direction=&fl_Owner=&fl_Goods=&fl_DeliveryType=&empty_filter=Y '
 # r = requests.get(url).json()
 # for item in r.get('rows'):
 #     print(item)
-
-
